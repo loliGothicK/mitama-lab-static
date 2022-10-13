69a27849e173ecb2aea8c92da4e5ae240a0d0937
Yuki WADA <yuki_wada@caddi.jp>
2022-10-13 10:19:36 +0900

    refactor: prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import type { AppProps } from 'next/app';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import * as React from "react";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import createEmotionCache from '../utility/createEmotionCache';
-import darkThemeOptions from '../styles/theme/darkThemeOptions';
-import '../styles/globals.css';
+import createEmotionCache from "../utility/createEmotionCache";
+import darkThemeOptions from "../styles/theme/darkThemeOptions";
+import "../styles/globals.css";
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
