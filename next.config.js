46f678d200fd61c3ead73f4b7fdfde9b4c79e767
Yuki WADA <yuki_wada@caddi.jp>
2022-10-22 02:37:04 +0900

    fix: exportPathMap

--- next.config.js
+++ next.config.js
@@ -5,7 +5,17 @@ const nextConfig = {
   swcMinify: true,
   exportPathMap: async () => {
     return {
-      "/": { page: "/" },
+      '/': { page: '/' },
+      '/mitamatch-operations': { page: '/mitamatch-operations' },
+      '/mitamatch-operations/install-app': {
+        page: '/mitamatch-operations/install-app',
+      },
+      '/mitamatch-operations/getting-started': {
+        page: '/mitamatch-operations/getting-started',
+      },
+      '/mitamatch-operations/features/v0_1_10': {
+        page: '/mitamatch-operations/features/v0_1_10',
+      },
     };
   },
 };
