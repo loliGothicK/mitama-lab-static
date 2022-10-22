254b73111cc9aa8b1aa38a0e7b406999900d8dda
Yuki WADA <yuki_wada@caddi.jp>
2022-10-23 01:08:34 +0900

    feat: bump v0.1.11

--- contents/ReleaseNotes.tsx
+++ contents/ReleaseNotes.tsx
@@ -12,6 +12,13 @@ const releaseNote = z.object({
 type ReleaseNote = z.infer<typeof releaseNote>;
 
 const ReleaseNotes: { [K: Version]: ReleaseNote } = {
+  'v0.1.11': releaseNote.parse({
+    wahtsNew: ['ライトモード/ダークモードに対応'],
+    bufFix: ['初回ロード時のクラッシュを修正'],
+    packages: {
+      zip: 'https://storage.cloud.google.com/mitamatch-operations-installer/MitamatchOperations%20(Package)_0.1.11.0_Test.zip',
+    },
+  }),
   'v0.1.10': releaseNote.parse({
     wahtsNew: ['オーダー自動編成', 'レギオンメンバー管理', '所持オーダー管理'],
     bufFix: ['細かいバグ修正を行いました'],
