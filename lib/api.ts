afb79d14c0b72ec3eb31b13ab64d1e1578a13ba7
Yuki WADA <yuki_wada@caddi.jp>
2023-04-06 08:24:48 +0900

    feat: prettier

--- lib/api.ts
+++ lib/api.ts
@@ -39,7 +39,10 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
 
 export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs();
-  return slugs.map(slug => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return (
+    slugs
+      .map(slug => getPostBySlug(slug, fields))
+      // sort posts by date in descending order
+      .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+  );
 }
