f8e55cb1a52ba8c9304463a881ab9ed88593b0a5
Yuki WADA <yuki_wada@caddi.jp>
2023-04-13 18:54:34 +0900

    feat: OG Image

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,28 +1,28 @@
-export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 // PV 数の計測
 export const pageview = (url: string) => {
   if (!GA_ID) return;
-  window.gtag("config", GA_ID, {
+  window.gtag('config', GA_ID, {
     page_path: url,
   });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({
-                        action,
-                        category,
-                        label,
-                        value,
-                      }: {
+  action,
+  category,
+  label,
+  value,
+}: {
   action: string;
   category: string;
   label: string;
   value?: number;
 }): void => {
   if (!GA_ID) return;
-  window.gtag("event", action, {
+  window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
