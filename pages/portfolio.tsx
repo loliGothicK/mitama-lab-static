68ac33f048f210135da7b263eb4d2bcfe6e5565e
mitama <mitama@mitama.tech>
2023-12-07 23:38:16 +0900

    feat: responsive

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -52,64 +52,63 @@ const rows = [
   createData(2012, 3, '大阪工業大学 工学部 環境工学科 卒業'),
   createData(2012, 4, '大阪府立高校 教員採用'),
   createData(2018, 3, '大阪府立高校 教員退職'),
-  createData(2018, 9, 'キャディ株式会社 入社（アルゴリズムエンジニア/3D CAD）'),
-  createData(2020, 2, 'キャディ株式会社 2D 図面 解析（画像認識）'),
-  createData(2021, 3, 'キャディ株式会社 バックエンドエンジニア（Rust）'),
-  createData(2023, 4, 'キャディ株式会社 フロントエンド（シナリオテスト）'),
+  createData(2018, 9, 'キャディ株式会社 入社'),
+  createData(2020, 2, 'キャディ株式会社/画像認識'),
+  createData(2021, 3, 'キャディ株式会社/バックエンド'),
+  createData(2023, 4, 'キャディ株式会社/フロントエンド'),
   createData(2023, 8, 'キャディ株式会社 退職'),
-  createData(2023, 8, '株式会社 Arent 入社（アルゴリズムエンジニア）'),
+  createData(2023, 8, '株式会社 Arent 入社'),
   createData(2023, 11, '株式会社 Arent 退職（うつ病のため）'),
 ];
 
-export default function Portfolio() {
+function Content() {
   return (
-    <MitamaLab>
-      <Container maxWidth={'xs'}>
-        <Typography variant="h3" component="div" gutterBottom align={'center'}>
-          {"Mitama's Portfolio"}
-        </Typography>
-        <Grid container spacing={2}>
-          <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            slidesPerView={1} //一度に表示するスライドの数
-            navigation //スライドを前後させるためのボタン、スライドの左右にある
-            loop={true}
-            centeredSlides={true}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
-          >
-            {JobList.map(({ title, span, stack, abstract }, index) => {
-              return (
-                <SwiperSlide key={`${index}`}>
-                  <Card sx={{ marginLeft: 10, marginRight: 10 }}>
-                    <CardContent>
-                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        {span}
-                      </Typography>
-                      <Typography variant="h5" component="div">
-                        {title}
-                      </Typography>
-                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        {stack}
-                      </Typography>
-                      <Typography variant="body2">
-                        {abstract}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Read More</Button>
-                    </CardActions>
-                  </Card>
-                </SwiperSlide>
-              )
-            })}
-          </Swiper>
-        </Grid>
-        <Divider sx={{ margin: 10 }} />
-        <Box sx={{ overflow: "auto", marginBottom: 10 }}>
-          <Box sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
+    <>
+      <Typography variant="h3" component="div" gutterBottom align={'center'} margin={10}>
+        {"Mitama's Portfolio"}
+      </Typography>
+      <Grid container spacing={2}>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          slidesPerView={1} //一度に表示するスライドの数
+          navigation //スライドを前後させるためのボタン、スライドの左右にある
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+        >
+          {JobList.map(({ title, span, stack, abstract }, index) => {
+            return (
+              <SwiperSlide key={`${index}`}>
+                <Card sx={{ marginLeft: 10, marginRight: 10 }}>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                      {span}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      {title}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {stack}
+                    </Typography>
+                    <Typography variant="body2">
+                      {abstract}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Read More</Button>
+                  </CardActions>
+                </Card>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </Grid>
+      <Divider sx={{ margin: 10 }} />
+      <Box sx={{ overflow: "auto", marginBottom: 10 }}>
+        <Box sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table" align={'center'}>
               <TableHead>
@@ -133,8 +132,20 @@ export default function Portfolio() {
               </TableBody>
             </Table>
           </TableContainer>
-          </Box>
         </Box>
+      </Box>
+    </>
+  );
+}
+
+export default function Portfolio() {
+  return (
+    <MitamaLab>
+      <Container maxWidth={'xs'} sx={{ display:{ xs:'block', sm:'none' } }}>
+        <Content />
+      </Container>
+      <Container maxWidth={'sm'} sx={{ display:{ xs:'none', sm:'block' } }}>
+        <Content />
       </Container>
     </MitamaLab>
   );
