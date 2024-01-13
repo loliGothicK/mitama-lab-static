173dca93df443d8b73604c97c057a24ff461b123
mitama <loligothick@gmail.com>
2024-01-13 23:18:52 +0900

    feat: UI

--- pages/allb_timer.tsx
+++ pages/allb_timer.tsx
@@ -77,7 +77,7 @@ const TimerComponent = ({ interval }: {interval: number}) => {
       }
       return --prevCount;
     });
-  }, []);
+  }, [interval]);
   useAnimationFrame(isRunning, countDown);
   
   return (
