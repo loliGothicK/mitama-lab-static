a8731350edb3330f204bba6c9486474cb6fbdba7
mitama <loligothick@gmail.com>
2024-01-13 22:25:25 +0900

    feat: speech

--- pages/allb_timer.tsx
+++ pages/allb_timer.tsx
@@ -2,6 +2,36 @@ import MitamaLab from "../layouts/MitamaLab";
 import {Container} from "@mui/material";
 import {useCallback, useEffect, useRef, useState} from "react";
 
+interface TextToSpeechInput {
+  text: string;
+  lang: "en-US" | "ja-JP"; // ISO 639-1コードの形式
+  speed?: number; // 範囲: 0.1 ~ 10
+  pitch?: number; // 範囲: 0.1 ~ 10
+  volume?: number; // 範囲: 0 ~ 1
+  voiceName?: string; // 使用する音声の種類
+}
+
+/**
+ * 特定の文字をしゃべらせる
+ * @param props
+ */
+export const playTextToSpeech = (props: TextToSpeechInput) => {
+  const utterance = new SpeechSynthesisUtterance(props.text);
+  utterance.rate = props.speed? props.speed: 1;
+  if (props.voiceName){
+    if (
+      speechSynthesis.getVoices().find(
+        (voice) => voice.name === props.voiceName)
+    ){
+      utterance.voice = speechSynthesis.getVoices().find(
+        (voice) => voice.name === props.voiceName
+      ) as SpeechSynthesisVoice
+    }
+  }
+  utterance.pitch = (props.pitch)? props.pitch: 1;
+  utterance.volume = (props.volume)? props.volume: 1;
+  speechSynthesis.speak(utterance);
+}
 const useAnimationFrame = (isRunning: boolean, callback = () => {}) => {
   const reqIdRef = useRef(0);
   const loop = useCallback(() => {
@@ -10,7 +40,6 @@ const useAnimationFrame = (isRunning: boolean, callback = () => {}) => {
   
   useEffect(() => {
     const id = setInterval(() => {
-      console.log(`isRunning: ${isRunning}`);
       if (isRunning) {
         reqIdRef.current = requestAnimationFrame(loop);
       }
@@ -24,11 +53,23 @@ const useAnimationFrame = (isRunning: boolean, callback = () => {}) => {
 };
 
 const TimerComponent = () => {
-  const [counter, setCounter] = useState(30);
+  const [counter, setCounter] = useState(40);
   const [isRunning, setIsRunning] = useState(false);
   
   const countDown = useCallback(() => {
-    setCounter(prevCount => --prevCount);
+    setCounter(prevCount => {
+      if (prevCount-1 === 30) {
+        playTextToSpeech({ text: "30 seconds left.", lang: "en-US"} satisfies TextToSpeechInput);
+      }
+      if (prevCount-1 === 10) {
+        playTextToSpeech({ text: "10 seconds left.", lang: "en-US" } satisfies TextToSpeechInput);
+      }
+      if (prevCount-1 === 0) {
+        setIsRunning(false);
+        return 30;
+      }
+      return --prevCount;
+    });
   }, []);
   useAnimationFrame(isRunning, countDown);
   
